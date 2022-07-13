cbfa758ac17cceffc1a224571c90cbd00f2217e0
virajchavda <virajchavda1807@gmail.com>
2022-07-13 14:26:11 +0530

    Source code updated

---
+++ PIR_with_esp8266/PIR_with_esp8266.ino
@@ -0,0 +1,43 @@
+#define timeSeconds 1
+
+// Set GPIOs for LED and PIR Motion Sensor
+const int led = 12;
+const int motionSensor = 13;
+
+// Timer: Auxiliary variables
+unsigned long now = millis();
+unsigned long lastTrigger = 0;
+boolean startTimer = false;
+
+// Checks if motion was detected, sets LED HIGH and starts a timer
+ICACHE_RAM_ATTR void detectsMovement() {
+  Serial.println("MOTION DETECTED!!!");
+  digitalWrite(led, HIGH);
+  startTimer = true;
+  lastTrigger = millis();
+}
+
+void setup() {
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  digitalWrite(led, HIGH);
+  // PIR Motion Sensor mode INPUT_PULLUP
+  pinMode(motionSensor, INPUT_PULLUP);
+  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
+  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
+
+  // Set LED to LOW
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+}
+
+void loop() {
+  // Current time
+  now = millis();
+  // Turn off the LED after the number of seconds defined in the timeSeconds variable
+  if(startTimer && (now - lastTrigger > (timeSeconds*1000))) {
+    Serial.println("Motion stopped...");
+    digitalWrite(led, LOW);
+    startTimer = false;
+  }
+}
